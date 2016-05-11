deee275e321e2077a881c52c49f8ca603e9fddf2
Scot Hacker <shacker@birdhouse.org>
2016-05-11 00:14:29 -0700

    PEP-8

--- people/models.py
+++ people/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import AbstractUser
 from django.template.defaultfilters import slugify
 from sorl.thumbnail import ImageField
 
+
 def get_avatar_path(instance, filename):
     """
     Clean the filename and determine the foldername to upload avatars to.
@@ -15,10 +16,12 @@ def get_avatar_path(instance, filename):
     parts = str(filename).split(".")
     return 'avatars/' + instance.username + '/' + slugify(parts[0]) + '.' + parts[1]
 
+
 def validate_username(username):
     if " " in username:
         msg = 'Usernames cannot contain spaces'
         raise ValidationError(msg)
+
 
 class UserProfile(AbstractUser):
     """
@@ -29,13 +32,13 @@ class UserProfile(AbstractUser):
         help_text='Describe yourself: Interests, Bio, etc. Limited to 500 words.', null=True, blank=True)
     twitter = models.CharField(
         max_length=60, help_text='Your Twitter username',
-        null=True, blank=True, validators = [validate_username, ])
+        null=True, blank=True, validators=[validate_username, ])
     facebook = models.CharField(
         max_length=60, help_text='Your Facebook username',
-        null=True, blank=True, validators = [validate_username, ])
+        null=True, blank=True, validators=[validate_username, ])
     instagram = models.CharField(
         max_length=60, help_text='Your Instagram username',
-        null=True, blank=True, validators = [validate_username, ])
+        null=True, blank=True, validators=[validate_username, ])
     photo = ImageField(upload_to=get_avatar_path, blank=True, null=True, max_length=255)
 
     @property
