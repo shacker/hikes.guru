eee4d5a5d4ebc0374496ee9a304bfd08e756bacc
Scot Hacker <shacker@birdhouse.org>
2016-05-15 10:10:26 -0700

    Add favicon and png

--- project/urls.py
+++ project/urls.py
@@ -3,12 +3,16 @@ from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth.views import logout
+from django.views.generic.base import RedirectView
+
 
 from hikes.views import home
 from feedback.views import feedback
 
 
 urlpatterns = [
+    url(r'^favicon\.ico$', RedirectView.as_view(url='/static/img/favicon.ico', permanent=True)),
+
     url('', include('social.apps.django_app.urls', namespace='social')),  # social_auth
     url(r'^logout/$', logout, {'next_page': '/'}, name="logout"),
     url(r'^feedback/$', feedback, name='feedback'),
