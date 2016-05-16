5a64b6e50f76bdd6e5b271c65af5afca648e74dd
Scot Hacker <shacker@birdhouse.org>
2016-05-15 23:23:33 -0700

    Add generic avatar, clean up profile detail, add ckeditor

---
+++ project/static/js/ckeditor_config.js
@@ -0,0 +1,19 @@
+CKEDITOR.editorConfig = function( config ) {
+	config.toolbarGroups = [
+		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+		{ name: 'styles', groups: [ 'styles' ] },
+		{ name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
+		{ name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
+		{ name: 'links', groups: [ 'links' ] },
+		{ name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi', 'paragraph' ] },
+		{ name: 'insert', groups: [ 'insert' ] },
+		{ name: 'forms', groups: [ 'forms' ] },
+		{ name: 'tools', groups: [ 'tools' ] },
+		{ name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
+		{ name: 'others', groups: [ 'others' ] },
+		{ name: 'colors', groups: [ 'colors' ] },
+		{ name: 'about', groups: [ 'about' ] }
+	];
+
+	config.removeButtons = 'Underline,Subscript,Superscript,About,Cut,RemoveFormat,Scayt,Image,Maximize,Copy,Paste,PasteText,PasteFromWord,Anchor,Table,Outdent,Indent,Strike,Styles,HorizontalRule,SpecialChar';
+};
