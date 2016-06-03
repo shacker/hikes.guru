455e450acc6cedf8c2b1f5bcf03d1aa3772f3663
Scot Hacker <shacker@birdhouse.org>
2016-06-03 00:30:23 -0700

    Region is a required attribute

--- apps/trails/models.py
+++ apps/trails/models.py
@@ -27,7 +27,7 @@ class Trail(models.Model):
 
     owner = models.ForeignKey(UserProfile)
     title = models.CharField(max_length=120)
-    region = models.CharField(max_length=120, help_text="e.g. Yosemite, CA", blank=True, null=True)
+    region = models.CharField(max_length=120, help_text="e.g. Yosemite, CA")
     description = models.TextField(blank=True, null=True)
     created = models.DateTimeField(auto_now=True)
     updated = models.DateTimeField(auto_now_add=True)
