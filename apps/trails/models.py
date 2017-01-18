185ae0fcd728753e16dc54bcabfc1b92e6f658dc
Scot Hacker <shacker@birdhouse.org>
2017-01-17 23:53:53 -0800

    Better help text for public/private trails

--- apps/trails/models.py
+++ apps/trails/models.py
@@ -57,7 +57,10 @@ class Trail(models.Model):
     calories = models.PositiveSmallIntegerField(blank=True, null=True)
     duration = models.DurationField(blank=True, null=True)
     geocaches = models.BooleanField(default=False, help_text="Geocaches available on this trail")
-    public = models.BooleanField(default=True, help_text="Visible to the world")
+    public = models.BooleanField(
+        default=True, help_text="The strength of this site is its role as a public directory.\
+        We encourage users to log all entries as public.\
+        Non-public trails are visible only to you.")
     featured = models.BooleanField(default=False, help_text="Editor picks")
     trail_type = models.CharField(max_length=6, choices=(TRAIL_TYPE_CHOICES), default="loop")
     activity_type = models.CharField(max_length=6, choices=(ACTIVITY_TYPE_CHOICES), default="hike")
